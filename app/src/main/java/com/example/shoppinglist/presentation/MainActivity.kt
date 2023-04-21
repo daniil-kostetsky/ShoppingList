ef83586d25bfbd1b1fde742f1f152dde96826118
Daniil Kostetsky <daniilkostetsky@gmail.com>
2023-04-21 18:02:42 +0300

    Implementation list with LinearLayout

--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -2,29 +2,51 @@ package com.example.shoppinglist.presentation
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View.inflate
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.shoppinglist.R
+import com.example.shoppinglist.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mainViewModel: MainViewModel
 
-    private var count = 0
+    private lateinit var linearLayout: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        linearLayout = findViewById(R.id.ll_shop_list)
 
         mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
         mainViewModel.shopList.observe(this, Observer {
-            Log.d("MainActivityTest", it.toString())
-            if (count == 0) {
-                count++
-                mainViewModel.changeEnableState(it[0])
-            }
+            showList(it)
         })
+    }
 
+    private fun showList(list: List<ShopItem>) {
+        linearLayout.removeAllViews()
+        for (shopItem in list) {
+            val layoutId = if (shopItem.enabled) {
+                R.layout.item_shop_enabled
+            } else {
+                R.layout.item_shop_disabled
+            }
+            val view = LayoutInflater.from(this).inflate(layoutId, linearLayout, false)
+            val tvName = view.findViewById<TextView>(R.id.tv_product_name)
+            val tvCount = view.findViewById<TextView>(R.id.tv_count)
+            tvName.text = shopItem.name
+            tvCount.text = shopItem.count.toString()
+            view.setOnLongClickListener {
+                mainViewModel.changeEnableState(shopItem)
+                true
+            }
+            linearLayout.addView(view)
+        }
     }
 }
